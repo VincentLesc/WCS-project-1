53e5d94baad968433a5c9ffbfdc8402f64d27923
vincent LES <vincent.leschaeve@gmail.com>
2018-09-12 15:30:49 +0200

    new page Euratech

---
+++ euratech.php
@@ -0,0 +1,71 @@
+<?php
+include 'navbar.html';
+
+?>
+    <section class="container">
+        <div class="row">
+            
+        </div>
+    </section>
+
+    <hr>
+    <section class="container">
+        <div class="row p-4">
+            <h2>Events</h2>
+        </div>
+        <div class="row justify-content-between pb-4">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="https://via.placeholder.com/350x250" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="https://via.placeholder.com/350x250" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="https://via.placeholder.com/350x250" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section class="container">
+        <div class="row p-4">
+            <h2>Find us in Euratech !</h2>
+        </div>
+        <div class="row pb-4 justify-content-between">
+            <div class="col-7">
+                <img src="https://via.placeholder.com/550x250" alt="" class="img-fluid">
+            </div>
+            <div class="col-5">
+                <div class="card h-100">
+                    <div class="card-header">Header</div>
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Dark card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+include 'footer.html';
