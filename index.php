a27aaae86fee0ff13457d6099bf3c18da7061800
vincent LES <vincent.leschaeve@gmail.com>
2018-09-14 14:59:07 +0200

    Add link to contact button.

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
             Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. <br/>
             Maecenas adipiscing ante non diam sodales hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. <br/>Pellentesque congue. Ut in risus volutpat libero pharetra tempor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.</p>
         <div class="text-center">
-            <button id="button" type="button" class="btn btn-outline-dark btn btn-primary btn-lg"><a href="form.php">Contact us</a></button>
+            <a href="form.php"><button id="button" type="button" class="btn btn-outline-dark btn btn-primary btn-lg">Contact us</button></a>
         </div>
         <h2 class="contact_text"><strong>You want to join Wild Code School army ?<br/>
                 Click on the button above, fill in the informations<br/> and discover the joy of web development !</strong></h2>
