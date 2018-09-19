3b27afda1c4ec198edb01cc63f1d9f57ca1d9238
vincent LES <vincent.leschaeve@gmail.com>
2018-09-19 15:47:42 +0200

    Adding img for slider

--- public/euratech.php
+++ public/euratech.php
@@ -3,13 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <title>EuraDark Arena</title>
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Wild Code School</title>
+    <link rel="stylesheet" href="../src/style/style.css">
 </head>
 <body>
-<?php require 'navbar.html'; ?>
-    <section class="container">
+
+// require navbar
+<?php require '../src/app/navbar.html'; ?>
+
+<section id="euraslider">
+    <div class="container">
         <div class="row p-4 justify-content-center">
             <h2>Title Euratechnologies</h2>
         </div>
@@ -17,19 +24,16 @@
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="https://via.placeholder.com/1400x500" alt="First slide">
+                        <img class="d-block w-100" src="../src/Images/euratech/slidereura1.jpg" alt="First slide">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porta est a nunc fermentum imperdiet. Morbi in
-                            ex sed neque convallis accumsan id nec ante. Donec condimentum neque justo, et facilisis justo pharetra eu.</p>
+                            ex sed neque convallis accumsan id nec ante. Donec condimentum neque justo, et facilisis justo pharetra eu.
+                        </p>
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="https://via.placeholder.com/1400x500" alt="Second slide">
+                        <img class="d-block w-100" src="../src/Images/euratech/slidereura2.png" alt="Second slide">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porta est a nunc fermentum imperdiet. Morbi in
-                            ex sed neque convallis accumsan id nec ante. Donec condimentum neque justo, et facilisis justo pharetra eu.</p>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="https://via.placeholder.com/1400x500" alt="Third slide">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porta est a nunc fermentum imperdiet. Morbi in
-                            ex sed neque convallis accumsan id nec ante. Donec condimentum neque justo, et facilisis justo pharetra eu.</p>
+                            ex sed neque convallis accumsan id nec ante. Donec condimentum neque justo, et facilisis justo pharetra eu.
+                        </p>
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -42,17 +46,21 @@
                 </a>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <hr>
-    <section class="container">
+<hr>
+
+<section id="events">
+    <div class="container">
         <div class="row p-4 justify-content-center">
             <h2>Events</h2>
         </div>
         <div class="row justify-content-between pb-4">
             <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top" src="https://via.placeholder.com/350x250" alt="Card image cap">
+                <div class="card events">
+                    <img class="card-img-top event" id="event" src="../src/Images/euratech/veetevent.png" alt="Card image cap">
+                    <img class="card-img-top event" id="eventhidden" src="../src/Images/euratech/veeteventover.png" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -70,7 +78,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col">
                 <div class="card">
                     <img class="card-img-top" src="https://via.placeholder.com/350x250" alt="Card image cap">
                     <div class="card-body">
@@ -81,9 +89,13 @@
                 </div>
             </div>
         </div>
-    </section>
-    <hr>
-    <section class="container">
+    </div>
+</section>
+
+<hr>
+
+<section id="locate">
+    <div class="container">
         <div class="row p-4 justify-content-center">
             <h2>Find us in Euratech !</h2>
         </div>
@@ -101,9 +113,13 @@
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-<?php require 'footer.html'; ?>
+// require footer
+
+<?php require '../src/app/footer.html'; ?>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
