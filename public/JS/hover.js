0c4adff7d99ea028a2392b9cd919b5ab6912c3a7
Maureen Vinchent <vinchent.maureen@gmail.com>
2018-09-20 13:57:17 +0200

    Last edit for wilders.php to add content, images and JS script.

--- public/JS/hover.js
+++ public/JS/hover.js
@@ -1,5 +1,4 @@
-// Hover on images
-
+// Hover on images in Instructors page
 $(".olivierImg").on({"mouseover" : function() {
     this.src = '../src/Images/instructors/olivgif.gif';
 }, 
@@ -23,3 +22,45 @@ $(".loicImg").on({"mouseover" : function() {
     this.src='../src/Images/instructors/loic.jpg';
   }
 });
+
+// Hover on images in Wilders page
+$(".vladi").on({"mouseover" : function() {
+    this.src = '../public/Images/wilders/vladijedi.jpg';
+}, 
+"mouseout" : function() {
+    this.src='../public/Images/wilders/vladimir.jpg';
+  }
+});
+
+$(".fleur").on({"mouseover" : function() {
+    this.src = '../public/Images/wilders/fleurjedi.jpg';
+}, 
+"mouseout" : function() {
+    this.src='../public/Images/wilders/fleur.jpg';
+  }
+});
+
+$(".vincent").on({"mouseover" : function() {
+    this.src = '../public/Images/wilders/vincentjedi.jpg';
+}, 
+"mouseout" : function() {
+    this.src='../public/Images/wilders/vincent.jpg';
+  }
+});
+
+$(".florian").on({"mouseover" : function() {
+    this.src = '../public/Images/wilders/florianjedi.jpg';
+}, 
+"mouseout" : function() {
+    this.src='../public/Images/wilders/florian.jpg';
+  }
+});
+
+$(".maureen").on({"mouseover" : function() {
+    this.src = '../public/Images/wilders/mojedi.jpg';
+}, 
+"mouseout" : function() {
+    this.src='../public/Images/wilders/mo.png';
+  }
+});
+
