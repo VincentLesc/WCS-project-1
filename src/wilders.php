7d0222775facb3412f36cc6566d3a355c71b01dc
Maureen Vinchent <vinchent.maureen@gmail.com>
2018-09-20 16:00:22 +0200

    Adding favicon in wilders.php file.

--- src/wilders.php
+++ src/wilders.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Wild Code School humoristic website">
-	<meta name="author" content="Maureen Vinchent">
+    <meta name="author" content="Maureen Vinchent">
+    <link rel="shortcut icon" href="../public/Images/favicon.ico">
     <link rel="stylesheet" href="../public/CSS/bootstrap.min.css">
     <link rel="stylesheet" href="../public/CSS/bootstrap-grid.min.css">
     <link rel="stylesheet" href="../public/CSS/bootstrap-reboot.min.css">
